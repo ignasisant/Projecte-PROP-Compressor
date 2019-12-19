361b710a2457283bad2693689a4023576cd21c0c
joaquim.ferrer <joaquim.ferrer@est.fub.upc.edu>
2019-12-19 20:19:16 +0100

    Afegint getData

--- FONTS/domini/testsDelQuim/jpegDriver.java
+++ FONTS/domini/testsDelQuim/jpegDriver.java
@@ -21,7 +21,7 @@ public class jpegDriver {
 
         Fitxer f = new Fitxer();
         //String fitxer = f.llegirFitxer("./ppms/blanc.ppm");
-        String fitxer = f.llegirFitxer("./ppms/blanc.ppm");
+        String fitxer = f.llegirFitxer("./out_sense_huffman.ppm");
         System.out.println("Començo a comprimir");
         System.out.println("Començo a comprimir");
         jpegCompressor j = new jpegCompressor();
