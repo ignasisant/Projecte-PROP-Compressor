0746641ce5af18fdefcb3967e415516b5eb4a62b
quim <cndsjkac>
2019-12-20 14:49:30 +0100

    Cardo un commit amb merge de la meva branca visca

--- FONTS/domini/PPMTooBig.java
+++ FONTS/domini/PPMTooBig.java
@@ -3,6 +3,7 @@ package domini;
 public class PPMTooBig extends Exception {
     public PPMTooBig() {
         super("El PPM és massa gran i pot petar la pila. No es podrà comprimir amb JPEG");
+
     }
 
 }
